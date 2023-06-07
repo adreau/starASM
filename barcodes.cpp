2d3f84b8b8384facdbe5fc4db5fd9eecfe9386b8
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2023-06-07 11:38:10 +0200

    Refactor barcodes into a single structure

---
+++ barcodes.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <iostream>
+#include "constants.h"
+#include "globals.h"
+#include "barcodes.h"
+
+unsigned int Barcodes::count_n_reads (unsigned long id) {
+  std::vector < unsigned long > names;
+  for (unsigned i = offsets[id]; i < offsets[id] + counts[id]; ++i) {
+    names.push_back(reads[i].name);
+  }
+  return count_different(names);
+}
+
+// Set to zero reads molecules with few reads
+void Barcodes::trim () {
+  std::cerr << TAB << "Removing outlier barcodes...\n";
+  unsigned int cpt = 0;
+  for (unsigned i = 0; i < ids.size(); ++i) {
+    if (count_n_reads(i) < Globals::min_n_reads_barcode) {
+      counts[i] = 0;
+      ++cpt;
+    }
+  }
+  std::cerr << TAB << TAB << cpt << " / " << ids.size() << " barcodes removed.\n";
+}
+
+void Barcodes::sort () {
+  std::cerr << TAB << "Sorting barcodes...\n";
+  for (unsigned i = 0; i < ids.size(); ++i) {
+    if (counts[i] > 1) {
+      std::sort(reads.begin() + offsets[i], reads.begin() + offsets[i] + counts[i]);
+    }
+  }
+}
+
+void Barcodes::set_structure () {
+  unsigned long size   = count_map.size();
+  unsigned int cpt     = 0;
+  unsigned long offset = 0;
+  offsets.reserve(size);
+  counts.reserve(size);
+  for (auto &it: count_map) {
+    offsets.push_back(offset);
+    counts.push_back(it.second);
+    offset       += it.second;
+    ids[it.first] = cpt;
+    ++cpt;
+  }
+  count_map.clear();
+  current_offsets = offsets;
+  reads = std::vector < Read > (offset);
+}
---
+++ barcodes.h
@@ -0,0 +1,23 @@
+#ifndef BARCODES_H
+#define BARCODES_H
+
+#include <string>
+#include <unordered_map>
+#include "read.h"
+
+
+struct Barcodes {
+  std::unordered_map < std::string, unsigned int > count_map;
+  std::unordered_map < std::string, unsigned int > ids;
+  std::vector < unsigned int >                     counts;
+  std::vector < unsigned int >                     offsets;
+  std::vector < unsigned int >                     current_offsets;
+  std::vector < Read >                             reads;
+
+  void          set_structure();
+  unsigned int  count_n_reads (unsigned long id);
+  void          trim();
+  void          sort();
+};
+
+#endif
